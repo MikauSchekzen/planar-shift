1e8d17ce7c24c9ceb1606585a527e60e484c4244
Tijmen Bok <mikauschekzen@gmail.com>
2015-10-27 09:30:04 +0100

    expanded upon Fort Hemsworth

--- js/plugins/MSS_CritDamage.js
+++ js/plugins/MSS_CritDamage.js
@@ -1,8 +1,20 @@
+var MikauSchekzen = MikauSchekzen || {};
+MikauSchekzen.Critical = MikauSchekzen.Critical || {};
+
 /*:
-	@plugindesc Changes the critical strike damage (currently only to 2x damage)
-	@author MikauSchekzen
+@plugindesc Changes the critical strike damage (and healing)
+@author MikauSchekzen
+
+@param Critical Multiplier
+@desc The multiplier of criticals
+Default 3
+@default 3
 */
 
+MikauSchekzen.Parameters = PluginManager.parameters('MSS_CritDamage');
+
+MikauSchekzen.Critical._critMultiplier = Number(MikauSchekzen.Parameters['Critical Multiplier']);
+
 Game_Action.prototype.applyCritical = function(damage) {
-    return damage * 2;
+    return damage * MikauSchekzen.Critical._critMultiplier;
 };
